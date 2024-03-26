3ac9eea9e568ba4bbd9c9028f9bc52001a9ea852
Marc Cenac <marc.cenac@datadoghq.com>
2024-03-26 17:15:34 -0500

    Add class for integration testing census catalog

---
+++ core/src/test/java/org/apache/iceberg/rest/TestCensusCatalog.java
@@ -0,0 +1,77 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package org.apache.iceberg.rest;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.catalog.SessionCatalog;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestCensusCatalog extends TestRESTCatalog {
+  private RESTCatalog restCatalog;
+
+  @BeforeEach
+  public void createCatalog() throws Exception {
+    String censusUri = "http://localhost:8080/catalog";
+
+    // in the future we may want to add user info here
+    SessionCatalog.SessionContext context = SessionCatalog.SessionContext.createEmpty();
+
+    this.restCatalog =
+        new RESTCatalog(
+            context,
+            (config) -> HTTPClient.builder(config).uri(config.get(CatalogProperties.URI)).build());
+    restCatalog.initialize(
+            "census",
+            ImmutableMap.of(CatalogProperties.URI, censusUri));
+  }
+
+  @AfterEach
+  public void closeCatalog() throws Exception {
+    if (restCatalog != null) {
+      restCatalog.close();
+    }
+  }
+
+
+  @Override
+  protected RESTCatalog catalog() {
+    return restCatalog;
+  }
+
+  @Override
+  protected boolean supportsNamespaceProperties() {
+    return false;
+  }
+
+  @Override
+  protected boolean supportsServerSideRetry() {
+    return false;
+  }
+
+  @Override
+  protected boolean supportsNestedNamespaces() {
+    return true;
+  }
+
+}
